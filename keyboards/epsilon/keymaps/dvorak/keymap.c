44634016109f90a99d194dbf8d573308d2320690
Abby Berkers <abbyberkers@gmail.com>
2024-03-07 17:50:29 +0100

    Move to home row mods

--- keyboards/epsilon/keymaps/dvorak/keymap.c
+++ keyboards/epsilon/keymaps/dvorak/keymap.c
@@ -4,10 +4,21 @@
 #include QMK_KEYBOARD_H
 
 // Aliases.
+// One shot mods
 #define OS_LGUI OSM(MOD_LGUI)
 #define OS_LALT OSM(MOD_LALT)
 #define OS_LCTL OSM(MOD_LCTL)
 #define OS_LSFT OSM(MOD_LSFT)
+
+#define LFT_GUI MT(MOD_LGUI, KC_A)
+#define LFT_ALT MT(MOD_LALT, KC_O)
+#define LFT_CTL MT(MOD_LCTL, KC_E)
+#define LFT_SFT MT(MOD_LSFT, KC_U)
+
+#define RGT_SFT MT(MOD_LSFT, KC_H)
+#define RGT_CTL MT(MOD_LCTL, KC_T)
+#define RGT_ALT MT(MOD_LALT, KC_N)
+#define RGT_GUI MT(MOD_LGUI, KC_S)
 
 #define CUT LCTL(KC_X)
 #define COPY LCTL(KC_C)
@@ -34,10 +45,10 @@ enum combos {
     THUMBS_MOUSE
 };
 
-const uint16_t PROGMEM sn_combo[] = {KC_S, KC_N, COMBO_END};
-const uint16_t PROGMEM th_combo[] = {KC_T, KC_H, COMBO_END};
-const uint16_t PROGMEM ao_combo[] = {KC_A, KC_O, COMBO_END};
-const uint16_t PROGMEM eu_combo[] = {KC_E, KC_U, COMBO_END};
+const uint16_t PROGMEM sn_combo[] = {RGT_GUI, RGT_ALT, COMBO_END};
+const uint16_t PROGMEM th_combo[] = {RGT_CTL, RGT_SFT, COMBO_END};
+const uint16_t PROGMEM ao_combo[] = {LFT_GUI, LFT_ALT, COMBO_END};
+const uint16_t PROGMEM eu_combo[] = {LFT_CTL, LFT_SFT, COMBO_END};
 const uint16_t PROGMEM lesc_combo[] = {KC_COMMA, KC_DOT, COMBO_END};
 const uint16_t PROGMEM resc_combo[] = {KC_R, KC_C, COMBO_END};
 const uint16_t PROGMEM mouse_combo[] = {KC_LSFT, KC_SPC, COMBO_END};
@@ -69,9 +80,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       *                       └───┘   └───┘
       */
     [DVORAK] = LAYOUT_split_3x6_3(
-           KC_ESC, KC_QUOT, KC_COMMA,   KC_DOT,    KC_P,    KC_Y,                            KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_BSPC,
-         KC_MINUS,    KC_A,     KC_O,     KC_E,    KC_U,    KC_I,                            KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_SLSH,
-       TG(QWERTY), KC_SCLN,     KC_Q,     KC_J,    KC_K,    KC_X,                            KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_EXLM,
+           KC_ESC, KC_QUOT, KC_COMMA,   KC_DOT,    KC_P,    KC_Y,                            KC_F,    KC_G,    KC_C,    KC_R,    KC_L, KC_BSPC,
+         KC_MINUS, LFT_GUI,  LFT_ALT,  LFT_CTL, LFT_SFT,    KC_I,                            KC_D, RGT_SFT, RGT_CTL, RGT_ALT, RGT_GUI, KC_SLSH,
+       TG(QWERTY), KC_SCLN,     KC_Q,     KC_J,    KC_K,    KC_X,                            KC_B,    KC_M,    KC_W,    KC_V,    KC_Z, KC_EXLM,
                                    TT(NAVIGATION), KC_LSFT, TT(NUMBERS),             TT(SYMBOLS),  KC_SPC, TT(FUNCTIONS)
     ),
     [QWERTY] = LAYOUT_split_3x6_3(
@@ -82,27 +93,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         ),
     [NUMBERS] = LAYOUT_split_3x6_3(
         _______, _______, KC_LCBR, KC_LPRN, KC_LBRC, _______,                          KC_PSLS,    KC_7,    KC_8,    KC_9, KC_PPLS, _______,
-        _______, OS_LGUI, OS_LALT, OS_LCTL, OS_LSFT, _______,                          KC_PAST,    KC_4,    KC_5,    KC_6, KC_PMNS, KC_X,
+        _______, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, _______,                          KC_PAST,    KC_4,    KC_5,    KC_6, KC_PMNS, KC_X,
         _______, _______, KC_RCBR, KC_RPRN, KC_RBRC, _______,                           KC_EQL,    KC_1,    KC_2,    KC_3, KC_CIRC, KC_UNDS,
                                             _______, _______, _______,         KC_DOT,  KC_SPC,    KC_0
     ),
     [SYMBOLS] = LAYOUT_split_3x6_3(
         KC_PERC, KC_BSLS,   KC_LT,   KC_GT,  KC_EQL,  KC_GRV,                          _______, KC_RBRC, KC_RPRN, KC_RCBR, _______, _______,
-        KC_CIRC, KC_SLSH, KC_LCBR, KC_LPRN, KC_LBRC, KC_MINS,                          _______, OS_LSFT, OS_LCTL, OS_LALT, OS_LGUI, _______,
+        KC_CIRC, KC_SLSH, KC_LCBR, KC_LPRN, KC_LBRC, KC_MINS,                          _______, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, _______,
         KC_ASTR, _______, _______, KC_NUHS, KC_PLUS, KC_EXLM,                          _______, _______, _______, _______, _______, _______,
-                                            _______,  KC_SPC, KC_LCTL,        _______, _______, _______
+                                            _______,  KC_SPC, _______,        _______, _______, _______
     ),
     [NAVIGATION] = LAYOUT_split_3x6_3(
         _______, _______, _______, _______, _______, _______,                          _______, KC_HOME,   KC_UP,  KC_END,  KC_INS, _______,
-        _______, OS_LGUI, OS_LALT, OS_LCTL, OS_LSFT, _______,                          _______, KC_LEFT, KC_DOWN, KC_RGHT,  KC_ENT, _______,
+        _______, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, _______,                          _______, KC_LEFT, KC_DOWN, KC_RGHT,  KC_ENT, _______,
         _______,    UNDO,     CUT,    COPY,   PASTE, _______,                          _______, KC_PGUP, KC_PGDN, _______, _______, _______,
-                                            _______, _______, _______,        _______, KC_LCTL, _______
+                                            _______, _______, _______,        _______, _______, _______
     ),
     [FUNCTIONS] = LAYOUT_split_3x6_3(
           KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F10,  KC_F11,                          _______, _______, _______, _______, _______, _______,
-          KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F12,                          _______, OS_LSFT, OS_LCTL, OS_LALT, OS_LGUI, _______,
+          KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F12,                          _______, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, _______,
         _______, _______, _______, _______, _______, _______,                          _______, _______, _______, _______, _______, _______,
-                                            _______, KC_LCTL, _______,        _______, _______, _______
+                                            _______, _______, _______,        _______, _______, _______
     ),
     [MOUSE] = LAYOUT_split_3x6_3(
         TG(MOUSE), _______, _______, _______, KC_BTN2, _______,                          _______, KC_WH_D, KC_MS_U, KC_WH_U, _______, _______,
