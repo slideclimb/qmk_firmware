9d43c68f53df0dd6e23c7f75fafdf9d92e2e5306
Abby Berkers <abbyberkers@gmail.com>
2024-03-10 09:43:31 +0100

    Small adjustments

--- keyboards/epsilon/keymaps/dvorak/keymap.c
+++ keyboards/epsilon/keymaps/dvorak/keymap.c
@@ -20,6 +20,9 @@
 #define RGT_ALT MT(MOD_LALT, KC_N)
 #define RGT_GUI MT(MOD_LGUI, KC_S)
 
+#define OPN_DBG LALT(KC_5)
+#define OPN_RUN LALT(KC_4)
+
 #define CUT LCTL(KC_X)
 #define COPY LCTL(KC_C)
 #define PASTE LCTL(KC_V)
@@ -51,7 +54,7 @@ const uint16_t PROGMEM ao_combo[] = {LFT_GUI, LFT_ALT, COMBO_END};
 const uint16_t PROGMEM eu_combo[] = {LFT_CTL, LFT_SFT, COMBO_END};
 const uint16_t PROGMEM lesc_combo[] = {KC_COMMA, KC_DOT, COMBO_END};
 const uint16_t PROGMEM resc_combo[] = {KC_R, KC_C, COMBO_END};
-const uint16_t PROGMEM mouse_combo[] = {KC_LSFT, KC_SPC, COMBO_END};
+const uint16_t PROGMEM mouse_combo[] = {QK_REP, KC_SPC, COMBO_END};
 
 
 combo_t key_combos[] = {
@@ -93,7 +96,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         ),
     [NUMBERS] = LAYOUT_split_3x6_3(
         _______, _______, KC_LCBR, KC_LPRN, KC_LBRC, _______,                          KC_PSLS,    KC_7,    KC_8,    KC_9, KC_PPLS, _______,
-        _______, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, _______,                          KC_PAST,    KC_4,    KC_5,    KC_6, KC_PMNS, KC_X,
+        _______, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_COMMA,                         KC_PAST,    KC_4,    KC_5,    KC_6, KC_PMNS, KC_X,
         _______, _______, KC_RCBR, KC_RPRN, KC_RBRC, _______,                           KC_EQL,    KC_1,    KC_2,    KC_3, KC_CIRC, KC_UNDS,
                                             _______, _______, _______,         KC_DOT,  KC_SPC,    KC_0
     ),
@@ -110,9 +113,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                             _______, _______, _______,        _______, _______, _______
     ),
     [FUNCTIONS] = LAYOUT_split_3x6_3(
-          KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F10,  KC_F11,                          _______, _______, _______, _______, _______, _______,
+          KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F10,  KC_F11,                          _______, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, _______,
           KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F12,                          _______, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, _______,
-        _______, _______, _______, _______, _______, _______,                          _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______,                          _______, OPN_DBG, OPN_RUN, _______, _______, _______,
                                             _______,  KC_TAB, _______,        _______, _______, _______
     ),
     [MOUSE] = LAYOUT_split_3x6_3(
